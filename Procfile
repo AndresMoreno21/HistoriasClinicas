11981561952eb357353d766367bdd28c22d68e5f
AndMorenoD <andmoreno@umariana.edu.co>
2019-01-04 19:54:13 -0500

    Version 1

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn serviciosClinicos.wsgi
--- serviciosClinicos/settings.py
+++ serviciosClinicos/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -178,3 +180,7 @@ if DEBUG:
 else:
     #Aqui se debe configrar el servidor EMAIL
     pass
+
+
+# Heroku Configurations
+django_heroku.settings(locals())
